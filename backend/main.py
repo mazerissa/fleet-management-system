9bed4f3c9f29383d11e14fb9ca127e0fbabd00ad
mazerissa <youcefelghoul@yahoo.com>
2026-08-07 20:43:18 +0200

    ading main code and configs

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .database.database import engine
-from .database.models import Base
-from .vehicles.routes import router as vehicles_router
-from .employees.routes import router as employees_router
+from backend.assignments.routes import router as assignments_router
+from backend.auth.routes import router as auth_router
+from backend.database.database import engine
+from backend.database.models import Base
+from backend.employees.routes import router as employees_router
+from backend.reports.routes import router as reports_router
+from backend.vehicles.routes import router as vehicles_router
 
-app = FastAPI(
-    title="Fleet Management API",
-    version="1.0.0"
-)
-
+app = FastAPI(title="Fleet Management API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,16 +18,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
 def root():
-    return {
-        "message": "Fleet Management API running"
-    }
+    return {"message": "Fleet Management API running"}
 
 
-app.include_router(vehicles_router)
+app.include_router(auth_router)
 app.include_router(employees_router)
+app.include_router(vehicles_router)
+app.include_router(assignments_router)
+app.include_router(reports_router)
